80d5aa07de2d72f82d8da2faf51670eda71216b0
Keith Smith <keith.smith@tvdsb.ca>
2020-08-30 14:37:08 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    int myAge = 28; //Change this to your age
+    String myName = "Mister Smith"; //Change this to your name
+   
+    //Change the output to one line. Use your notes to guide you
+    System.out.print("My Name is ");
+    System.out.print(myName);
+    System.out.print(" and I'm ");
+    System.out.print(myAge + " years old");
+  }
+}
